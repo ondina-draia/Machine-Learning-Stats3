f5be44704d79d319894ac04a3acc8835cc07824b
Mélina Baheux <m.baheux@yahoo.fr>
2020-01-08 13:57:15 +0100

    keras update

--- kerasscript.py
+++ kerasscript.py
@@ -11,9 +11,9 @@ import pandas as pd
 import seaborn as sns 
 import matplotlib.pyplot as plt 
 import numpy as np
-#~ from sklearn.feature_selection import SelectKBest, chi2, f_classif
+from sklearn.feature_selection import SelectKBest, chi2, f_classif
 #~ from sklearn.metrics import accuracy_score
-#~ from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split
 #~ from sklearn.preprocessing import StandardScaler
 
 
@@ -24,6 +24,35 @@ X = df.iloc[:,2:]
 dl = pd.read_csv('labels.csv')
 Y = dl.iloc[:,1]
 
+# Feature selection
+fts = SelectKBest(f_classif, k=12).fit(X, Y)
+# Obtention of a numpy.ndarray: contains bool like positions of the selected columns 
+support = fts.get_support()  
+# Feature filter 
+new_X = fts.transform(X)
+
 
 #Split training and testing data
 X_train, X_test, Y_train, Y_test = train_test_split(new_X,Y, test_size = 0.33, random_state=42)
+
+
+#~ X_train = np.reshape(X_train, (len(X_train), 28, 28, 1))
+#~ X_test = np.reshape(X_test, (len(X_test), 28, 28, 1))
+
+
+noise_factor = 0.5
+X_train_noisy = X_train + noise_factor * np.random.normal(loc=0.0, scale=1.0, size=X_train.shape)
+X_test_noisy = X_test + noise_factor * np.random.normal(loc=0.0, scale=1.0, size=X_test.shape)
+X_train_noisy = np.clip(X_train_noisy, 0., 1.)
+X_test_noisy = np.clip(X_test_noisy, 0., 1.)
+
+
+n = 5
+plt.figure(figsize=(20, 2))
+for i in range(n):
+	ax = plt.subplot(1, n, i+1)
+	#~ plt.imshow(X_test_noisy[i].reshape(6,6))
+	plt.gray()
+	ax.get_xaxis().set_visible(False)
+	ax.get_yaxis().set_visible(False)
+plt.show()
